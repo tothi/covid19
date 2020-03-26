70bd49d7c33e8d17cebbe7551c1bb6778fd4c8a7
tothi <tothi@users.noreply.github.com>
2020-03-26 02:33:08 +0100

    prettify plot
    Signed-off-by: tothi <tothi@users.noreply.github.com>

--- regression.py
+++ regression.py
@@ -38,10 +38,11 @@ def makeplot(df, dmax, trange, c, ylim=False):
         ax.plot(t, i(t, popt[0], popt[1]), symbol, label='regression using timeframe [0:{}]'.format(n))
         ax.legend()
 
-    ax.set(title = 'Impact of Epidemiological Measures (COVID-19 {})\n(early stage SIR exponential model)'.format(c))
-    ax.set(xlabel = 'days passed since outbreak / first registered case')
+    ax.set(title = 'Impact of Epidemiological Measures (COVID-19 {})\n(early stage SIR exponential model, calculated on {:%Y-%m-%d})'.format(c, df['date'][df.index[-1]]))
+    ax.set(xlabel = 'days passed since outbreak ({:%Y-%m-%d})'.format(df['date'][df.index[0]]))
     ax.set(ylabel = 'total number of infected individuals')
-
+    ax.axvline(x=trange[-1], color='r', ls=':')
+    
     if ylim:
         plt.ylim(ylim)
     plt.grid()
@@ -56,9 +57,12 @@ if __name__ == "__main__":
     # https://koronavirus.gov.hu/
     
     df = pd.read_csv('data-hun.csv', sep=';', names=['date', 'infectious', 'died', 'recovered', 'total', 'remark'], parse_dates=['date'])
-    p = makeplot(df, dmax=30, trange=range(6, 22, 5), c="Hungary")
+    p = makeplot(df, dmax=22, trange=range(6, 22, 5), c="Hungary", ylim=(0,300))
     p.savefig('plot-hun.png', dpi=150)
     p.show()
+    p1 = makeplot(df, dmax=30, trange=range(6, 22, 5), c="Hungary")
+    p1.savefig('plot-hun-large.png', dpi=150)
+    p1.show()
 
     # https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Spain
     df2 = pd.read_csv('data-spain.csv', sep=';', names=['date', 'time', 'AN','AR', 'AS', 'IB', 'CN', 'CB', 'CM', 'CL', 'CT', 'CE', 'VC', 'EX', 'GA', 'MD', 'ML', 'MU', 'NA', 'PV', 'RI', 'i_new', 'total', 'd_new', 'died', 'ICU', 'recovered', 'tested', 'refs'], parse_dates=['date'], converters={"total": atoi})
